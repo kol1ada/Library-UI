82e39cb57b4e4527cba0324fe57ca784aefbb8f6
Koliada Oleksii <koliada.oleksii@gmail.com>
2023-03-15 00:15:31 +0100

    +actions & +classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 import './lib/lib'
+import $ from './lib/lib'
 
-$('div').hide() // for Test
+$('div').hide().show() // for Test
+$('button').on('click', function () {
+    $(this).toggleClass('active')
+})
