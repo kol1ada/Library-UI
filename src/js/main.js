c83f996103259cbccfee1313497afae27ac2afc7
Koliada Oleksii <koliada.oleksii@gmail.com>
2023-03-15 15:54:26 +0100

    Change names: actions = handlers || +actions

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,13 @@ import $ from './lib/lib'
 
 $('div').hide().show() // for Test
 $('button').on('click', function () {
-    $(this).toggleClass('active')
+    // $(this).toggleClass('active')
+    $('div').eq(1).toggleClass('nobody')
 })
+
+$('div').click(function () {
+    console.log($(this).index())
+})
+
+console.log($('button').html('Hello'))
+console.log($('div').eq(2).find('.test'))
